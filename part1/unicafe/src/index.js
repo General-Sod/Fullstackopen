ff3eff1752a7459cd5255e998b67b042c3572ef1
shraja7 <shraja7@gmail.com>
2019-12-30 22:23:35 -0600

    full repo

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const goodHandler = () => {
+    setGood(good + 1);
+  };
+
+  const neutralHandler = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const badHandler = () => {
+    setBad(bad + 1);
+  };
+
+  const total = good + bad + neutral;
+  const avg = (good - bad) / total;
+  return (
+    <div>
+      <h1>Give feedback</h1>
+
+      <button onClick={goodHandler}>good</button>
+      <button onClick={neutralHandler}>neutral</button>
+      <button onClick={badHandler}>bad</button>
+
+      <h2>Statistics test</h2>
+
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {avg} </p>
+      <p>positive {good / total}%</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
