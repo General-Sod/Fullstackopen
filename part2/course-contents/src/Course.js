d6755eb8199e69072d264cbcb01b41c3da0d8376
shraja7 <shraja7@gmail.com>
2020-01-08 21:25:18 -0600

    adding arbitrary number of courses
    map through the overall course component in in the app comp.

--- part2/course-contents/src/Course.js
+++ part2/course-contents/src/Course.js
@@ -1,36 +1,30 @@
 import React from "react";
 
+const Header = ({ course }) => {
+  return <h1> {course.name}</h1>;
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((s, p) => p.exercises + s, 0);
+
+  return <p>total of {total} exercises</p>;
+};
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+);
+
 const Course = ({ course }) => {
-  const Total = ({ parts }) => {
-    const total = parts.reduce((s, p) => p.exercises + s, 0);
-    return (
-      <p>
-        Total number of exercises <b>{total}</b>{" "}
-      </p>
-    );
-  };
-
-  const Header = ({ parts }) => {
-    return <h1>{course.name}</h1>;
-  };
-
-  const Part = ({ part }) => {
-    return (
-      <p>
-        {part.name} {part.exercises}
-      </p>
-    );
-  };
-
-  const Content = ({ parts }) => {
-    return (
-      <div>
-        {parts.map(part => (
-          <Part key={part.id} part={part} />
-        ))}
-      </div>
-    );
-  };
   return (
     <div>
       <Header course={course} />
